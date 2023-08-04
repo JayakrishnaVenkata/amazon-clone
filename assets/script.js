2c2607442bb951e7460b7f28f39c8e6058cc0d8a
JayakrishnaVenkata <jayakrishna986634@gmail.com>
2023-08-04 22:20:48 +0530

    amazon

---
+++ assets/script.js
@@ -0,0 +1,46 @@
+function filter(){
+    console.log('came');
+    var dis=document.getElementById("hide-content");
+    console.log(dis);
+    if (dis.style.display === "none") {
+        console.log("display");
+        dis.style.display = "block";
+    } else {
+        console.log("display3");
+        dis.style.display = "none";
+    }
+}
+
+let slideindex=0;
+imgchange(slideindex);
+function imgchange(k){
+    Changeslide(slideindex+=k);
+}
+
+function Changeslide(n){
+    let display=document.querySelectorAll(".home-image-slider");
+    console.log(display);
+    let leng=display.length;
+    if(n>=leng){
+        n=0;
+        slideindex=0;
+    }
+    if(n<0){
+        n=leng-1;
+        slideindex=leng-1;
+    }
+    for(let i=0;i<leng;i++){
+        display[i].style.display="none";
+    }
+    display[n].style.display="block";
+}
+
+
+function moveScroll(n){
+    let scroll=document.querySelector(".x-contents-img").scrollLeft+=n;
+}
+
+function moveScroll2(n){
+    let scroll=document.querySelector(".internal-x-contents2").scrollLeft+=n;
+}
+
